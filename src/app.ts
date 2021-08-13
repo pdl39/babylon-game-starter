dfa7a6719d4b4ac62d78fe4404a1d2ebb9619be0
pdl39 <dhlee9111@gmail.com>
2021-08-13 01:58:55 -0400

    Add app.ts, tsconfig, and update webpack to handle Typescript

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import '@babylonjs/core/Debug/debugLayer';
+import '@babylonjs/inspector';
+import '@babylonjs/loaders/glTF';
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder
+} from '@babylonjs/core';
